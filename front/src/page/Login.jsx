e6c61e42c45bd193a9e1843ee5de870177704dde
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-01-28 13:56:37 +0900

    認証まわりの実装

--- front/src/page/Login.jsx
+++ front/src/page/Login.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+import { auth } from '../firebase';
+import theme from "./theme"
 
 function Login() {
+  const navigate = useNavigate();
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+    try {
+      await signInWithEmailAndPassword(auth, email, password).then(()=>{
+        console.log("login")
+        if (auth.currentUser.emailVerified) {
+          navigate('/');
+        } else {
+          console.log("メール認証ができていません。")
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            ログイン
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Eメール"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="パスワード"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              ログイン
+            </Button>
+            <Grid container justifyContent={"center"}>
+              <Grid item>
+                <Link href="register" variant="body2">
+                  {"ユーザー登録はこちらから"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default Login
