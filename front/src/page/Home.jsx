09ea1408e34cf8d5573c9feeb26c43fb4fe7f323
ikegami <masuten0912@gmail.com>
2023-01-28 20:05:18 +0900

    Home.jsxの作成

--- front/src/page/Home.jsx
+++ front/src/page/Home.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import { Box } from "@mui/system";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import db from "../firebase.js";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    // データベースからデータを取得する。
+    const postData = collection(db, "post");
+    getDocs(postData).then((snapShot) => {
+      setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
   return (
     <div>
       <header>
@@ -23,8 +35,7 @@ function Home() {
             variant="contained"
             component={Link}
             to="/profile"
-            sx={{ display: "flex"}}
-            size="small"
+            sx={{ display: "flex" }}
           >
             <p>新規投稿作成</p>
           </Button>
@@ -32,23 +43,33 @@ function Home() {
       </header>
 
       <body>
-        <br></br>
-        <Box
-          sx={{
-            border: 5,
-            borderColor: "primary.main",
-            borderRadius: 2,
-            width: "80%",
-            margin: "0 auto",
-          }}
-        >
-          <p>タイトル: タイトル</p>
-          <p>説明: 説明</p>
-          <p>使用言語: Python</p>
-          <p>時期: 時期</p>
-          <p>募集内容: 募集内容</p>
-          <p>SNS URL: </p>
-        </Box>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <br></br>
+            <Box
+              sx={{
+                border: 5,
+                borderColor: "primary.main",
+                borderRadius: 2,
+                width: "80%",
+                margin: "0 auto",
+              }}
+            >
+              <p>タイトル: {post.title}</p>
+              <p>説明: {post.description}</p>
+              {/* <p>時期: {post.time}</p> */}
+              <p>開発言語: {typeof(post.lang)}</p>
+
+              {/* {for (const [key, value] of Object.entries(post.lang)) {
+                return key
+              }} */}
+
+              <p>募集内容: {post.content}</p>
+              <p>SNS URL: {post.sns}</p>
+              {/* <button onClick={() => (deleteDoc(doc(db, 'post', post.id)))}>削除</button> */}
+            </Box>
+          </div>
+        ))}
       </body>
     </div>
   );
